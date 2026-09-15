619ae4ef5404cb680971753c618db66c137d593c
ode andi alamsyah <odeandialamsyah@gmail.com>
2026-09-15 09:21:28 +0700

    fix: add missing newlines at the end of several files

--- models/kategori_peralatan.go
+++ models/kategori_peralatan.go
@@ -7,13 +7,12 @@ import (
 )
 
 type KategoriPeralatan struct {
-	ID        uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	NamaKategori      string         `gorm:"column:nama_kategori;size:100;not null;unique" json:"nama_kategori"`
-	Description string         `gorm:"column:description;size:255" json:"description"`
-	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" json:"-"`
-
+	ID           uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	NamaKategori string         `gorm:"column:nama_kategori;size:100;not null;unique" json:"nama_kategori"`
+	Description  string         `gorm:"column:description;size:255" json:"description"`
+	CreatedAt    time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt    time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"column:deleted_at" json:"-"`
 }
 
 func (KategoriPeralatan) TableName() string {
--- models/ruangan.go
+++ models/ruangan.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Ruangan struct {
-	ID          uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	NamaRuangan string         `gorm:"column:nama_ruangan;size:150;not null;unique" json:"nama_ruangan"`
-	KodeRuangan string         `gorm:"column:kode_ruangan;size:50;not null;unique" json:"kode_ruangan"`
-	LantaiRuangan string	   `gorm:"column:lantai_ruangan;size:50;not null" json:"lantai_ruangan"`
-	LabsID      *uint64        `gorm:"column:labs_id" json:"labs_id"`
-	PICUserID   *uint64        `gorm:"column:pic_user_id" json:"pic_user_id"`
-	CreatedAt   time.Time      `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt   time.Time      `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at" json:"-"`
+	ID            uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	NamaRuangan   string         `gorm:"column:nama_ruangan;size:150;not null;unique" json:"nama_ruangan"`
+	KodeRuangan   string         `gorm:"column:kode_ruangan;size:50;not null;unique" json:"kode_ruangan"`
+	LantaiRuangan string         `gorm:"column:lantai_ruangan;size:50;not null" json:"lantai_ruangan"`
+	LabsID        *uint64        `gorm:"column:labs_id" json:"labs_id"`
+	PICUserID     *uint64        `gorm:"column:pic_user_id" json:"pic_user_id"`
+	CreatedAt     time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt     time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt     gorm.DeletedAt `gorm:"column:deleted_at" json:"-"`
 
 	// Relasi
 	Labs    *Labs `gorm:"foreignKey:LabsID;references:ID" json:"labs,omitempty"`
